11d53b10df28764f540532f102857e115dbefe33
Miyoshi-Ryota <m1yosh1.ry0t4@gmail.com>
2021-01-06 08:48:35 +0900

    add bgp open message strcture

--- src/bgp.rs
+++ src/bgp.rs
@@ -1,3 +1,12 @@
+use std::net::Ipv4Addr;
+
+enum BGPVersion{
+    V1,
+    V2,
+    V3,
+    V4,
+}
+
 struct BgpMessageHeader {
     length: u16,
     type_: BgpMessageType,
@@ -10,7 +19,28 @@ enum BgpMessageType {
     Keepalive,
 }
 
-struct BgpOpenMessage;
+struct BgpOpenMessage {
+    header: BgpMessageHeader,
+    version: BGPVersion,
+    my_autonomous_system: AutonomousSystemNumber,
+    hold_time: HoldTime,
+    bgp_identifier: Ipv4Addr,
+    optional_parameter_length: u8, // すでにパース後であるこのデータストラクチャには不要かも
+}
+
+struct OptionalParameter {
+    type_: BgpOpenMessageOptionalParameterType,
+    length: u8, // すでにパース後であるこのデータストラクチャには不要かも
+    value: Vec<u8>,
+}
+
+enum BgpOpenMessageOptionalParameterType {
+    i_dont_know_now, // あとでRFC3392をみておく。
+}
+
+struct HoldTime(u16);
+struct AutonomousSystemNumber(u16);
+
 struct BgpUpdateMessage;
 struct BgpNotificationMessage;
 struct BgpKeepaliveMessage;
