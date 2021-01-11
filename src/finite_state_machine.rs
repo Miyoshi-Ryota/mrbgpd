182a68ad0d22e617d27d3bc82bf3a23b02f32943
Miyoshi-Ryota <m1yosh1.ry0t4@gmail.com>
2021-01-12 03:10:13 +0900

    refactor event

--- src/finite_state_machine.rs
+++ src/finite_state_machine.rs
@@ -11,30 +11,28 @@ struct SessionAttribute {
     keepalive_time: SystemTime,
 }
 
-enum AdministrativeEvent {
-    ManualStart,
-    ManualStop,
+enum Event {
+    // Administrative Event
+    ManualStart, // Event 1
+    ManualStop, // Event 2
+    // TimerEvent
+    ConnectRetryTimerExpires, // Event 9
+    HoldTimerExpires, // Event 10
+    KeepaliveTimerExpires, // Event 11
+    //TcpConnectionBasedEvent
+    TcpCrAcked, // Event 16
+    TcpConnectionConfirmed, // Event 17
+    TcpConnectionFails, // Event 18
+    // BgpMessageBasedEvent
+    BgpOpen, // Event 19
+    BgpHeaderErr, // Event 21
+    BgpOpenMsgErr, // Event 22
+    NotifMsgVerErr, // Event 24
+    NotifMsg, // Event 25
+    KeepAliveMsg, // Event 26
+    UpdateMsg, // Event 27
+    UpdateMsgErr, // Event 28
+
 }
 
-enum TimerEvent {
-    ConnectRetryTimerExpires,
-    HoldTimerExpires,
-    KeepaliveTimerExpires,
-}
-
-enum TcpConnectionBasedEvent {
-    TcpCrAcked,
-    TcpConnectionConfirmed,
-    TcpConnectionFails,
-}
-
-enum BgpMessageBasedEvent {
-    BgpOpen,
-    BgpHeaderErr,
-    BgpOpenMsgErr,
-    NotifMsgVerErr,
-    NotifMsg,
-    KeepAliveMsg,
-    UpdateMsg,
-    UpdateMsgErr,
 }
