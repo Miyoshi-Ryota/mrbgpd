e786c39b3334f0377cec8142543fa6c0441c88b0
Miyoshi-Ryota <m1yosh1.ry0t4@gmail.com>
2021-01-29 22:16:02 +0900

    add debug code

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ fn main() {
         let mut buf = vec![];
         match fsm.tcp_connection.as_ref().unwrap().read_to_end(&mut buf) {
             Ok(_) => handle_packets(buf),
-            Err(_) => println!("no packets...: {:?}", buf),
+            Err(e) => println!("error: {}, no packets...: {:?}", e, buf),
         }
         thread::sleep(time::Duration::from_secs(1));
     }
