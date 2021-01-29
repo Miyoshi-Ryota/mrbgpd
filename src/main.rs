83d6ee2d101f522ffc1f0e1b6b1a67a1fc597eb1
Miyoshi-Ryota <m1yosh1.ry0t4@gmail.com>
2021-01-29 10:53:45 +0900

    add handle packet

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use mrbgpd::finite_state_machine::{fsm, Event};
 use mrbgpd::Config;
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::io::Read;
 use std::env;
 
+fn handle_connection(s: &mut TcpStream) {
+    let mut buf = [0u8; 1024];
+    s.read(&mut buf);
+    println!("{:?}", buf[0]);
+}
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::parse_args(args);
@@ -10,11 +16,19 @@ fn main() {
     let tcp_listener = TcpListener::bind("0.0.0.0:179").expect("port 179が使用できません。");
     let mut fsm = fsm::new(config);
     fsm.event_queue.push(Event::ManualStart);
-    loop {
-        println!("{:?}", fsm.get_state());
-        match fsm.event_queue.pop() {
-            Some(event) => fsm.handle_event(&event),
-            None => (),
+        for stream in tcp_listener.incoming() {
+            println!("{:?}", fsm.get_state());
+            match fsm.event_queue.pop() {
+                Some(event) => fsm.handle_event(&event),
+                None => (),
+            }    
+            match stream {
+                Ok(mut s) => {
+                    handle_connection(&mut s);
+                }
+                Err(e) => {
+                    println!("Connection Error")
+                }
+            }
         }
-    }
 }
