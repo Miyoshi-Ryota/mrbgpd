950aabf68a5a72f97946b810cf90fa7f2fbc8e68
Miyoshi-Ryota <m1yosh1.ry0t4@gmail.com>
2021-04-20 10:29:58 +0900

    debug code

--- src/routing.rs
+++ src/routing.rs
@@ -117,6 +117,7 @@ pub async fn lookup_network_route(ip_prefix: &IpPrefix) -> Result<Vec<RouteMessa
     let all_routes = get_all_ip_v4_routes().await.unwrap();
     let mut result = vec![];
     for route in all_routes {
+        println!("{:?}", &route);
         let (network_address, prefix_length) = route.destination_prefix().unwrap();
         let network_address = match network_address {
             IpAddr::V4(addr) => addr,
